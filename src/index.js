1c3fc003c534f6a81c749d95077d9fbdc56a5414
roamstory <kokoru58@naver.com>
2018-01-05 01:39:03 +0900

    유투브 검색 반환

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './component/search_bar';
 
 const API_KEY = 'AIzaSyA2XRSnlrRPxG8sgOmFy6J41-czJ4MuZow';  //Youtube API KEY
+
+YTSearch({key : API_KEY, term: 'surfboards'}, function (data) {
+    console.log(data);
+});
 
 // Create a new component. This component sould produce
 // some HTML
