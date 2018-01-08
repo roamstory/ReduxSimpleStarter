db070eebb4cf45a7f8d355feafc3f0ddcf62f597
roamstory <kokoru58@naver.com>
2018-01-08 17:40:08 +0900

    유투브 상세 및 유투브 선택 시 보여주기 그리고 CSS 수정

--- src/component/video_list_item.js
+++ src/component/video_list_item.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const VideoListItem = ({video}) => {
+const VideoListItem = ({video, onVideoSelect}) => {
      console.log(video);
     // const video = props.video;
     const imageUrl = video.snippet.thumbnails.default.url;
     return (
-        <li className="list-group-item">
+        <li onClick= {() => onVideoSelect(video)} className="list-group-item">
             <div className="video-list media">
                 <div className="media-left">
                     <img className="media-object" src={imageUrl} />
                 </div>
 
                 <div className="media-body">
-                    <div className="media-heding">{video.snippet.title}</div>
+                    <div className="media-heading">{video.snippet.title}</div>
                 </div>
             </div>
         </li>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import VideoList from './component/video_list';
-
+import VideoDetail from './component/video_detail';
 import SearchBar from './component/search_bar';
 
 const API_KEY = 'AIzaSyA2XRSnlrRPxG8sgOmFy6J41-czJ4MuZow';  //Youtube API KEY
@@ -11,18 +11,28 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos : [] };
+        this.state = { videos : [], selectedVideo : null };
+        this.videoSearch('영국남자')
 
-        YTSearch({key : API_KEY, term: '영국남자'},  (videos) => {
-            this.setState({ videos });
+    }
+
+    videoSearch(term) {
+        YTSearch({key : API_KEY, term: term },  (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
             // this.setState({ videos : videos});
         });
     }
     render() {
         return (
             <div>
-                <SearchBar />
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                <VideoDetail video={ this.state.selectedVideo }/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={ this.state.videos } />
             </div>
         );
     }
